709a93a4dd154769296e8d52891c90cf528e63d0
Øyvind Robertsen <oyvind@arcticstyle.net>
2016-05-31 00:10:07 +0200

    Set emacs font size depending on platform

--- .emacs.d/customization/ui.el
+++ .emacs.d/customization/ui.el
@@ -5,7 +5,7 @@
 ;; Show line numbers
 (global-linum-mode)
 
-    ;; You can uncomment this to remove the graphical toolbar at the top. After
+;; You can uncomment this to remove the graphical toolbar at the top. After
 ;; awhile, you won't need the toolbar.
 ;; (when (fboundp 'tool-bar-mode)
 ;;   (tool-bar-mode -1))
@@ -25,11 +25,17 @@
 
 ;; Set font
 
-(set-face-attribute 'default nil
-                    :family "Source Code Pro"
-                    :height 110
-                    :weight 'normal
-                    :width 'normal)
+(if (memq window-system '(mac ns))
+    (set-face-attribute 'default nil
+                        :family "Source Code Pro"
+                        :height 140
+                        :weight 'normal
+                        :width 'normal)
+  (set-face-attribute 'default nil
+                      :family "Source Code Pro"
+                      :height 110
+                      :weight 'normal
+                      :width 'normal))
 
 
 ;; Uncomment the lines below by removing semicolons and play with the
@@ -39,23 +45,23 @@
 
 ;; These settings relate to how emacs interacts with your operating system
 (setq ;; makes killing/yanking interact with the clipboard
-      x-select-enable-clipboard t
+ x-select-enable-clipboard t
 
-      ;; I'm actually not sure what this does but it's recommended?
-      x-select-enable-primary t
+ ;; I'm actually not sure what this does but it's recommended?
+ x-select-enable-primary t
 
-      ;; Save clipboard strings into kill ring before replacing them.
-      ;; When one selects something in another program to paste it into Emacs,
-      ;; but kills something in Emacs before actually pasting it,
-      ;; this selection is gone unless this variable is non-nil
-      save-interprogram-paste-before-kill t
+ ;; Save clipboard strings into kill ring before replacing them.
+ ;; When one selects something in another program to paste it into Emacs,
+ ;; but kills something in Emacs before actually pasting it,
+ ;; this selection is gone unless this variable is non-nil
+ save-interprogram-paste-before-kill t
 
-      ;; Shows all options when running apropos. For more info,
-      ;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Apropos.html
-      apropos-do-all t
+ ;; Shows all options when running apropos. For more info,
+ ;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Apropos.html
+ apropos-do-all t
 
-      ;; Mouse yank commands yank at point instead of at click.
-      mouse-yank-at-point t)
+ ;; Mouse yank commands yank at point instead of at click.
+ mouse-yank-at-point t)
 
 ;; No cursor blinking, it's distracting
 (blink-cursor-mode 0)
