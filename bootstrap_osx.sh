b214e8b4e69307675f646e0b50a663def38c007d
Øyvind Robertsen <oyvind@arcticstyle.net>
2017-08-09 15:14:25 +0200

    Remove old emacs and neovim config, replace with spacemacs

--- bootstrap_osx.sh
+++ bootstrap_osx.sh
@@ -15,7 +15,10 @@ create_symlink() {
 }
 
 setup_shell() {
-  create_symlink bash/.bashrc ~/
+  brew install zsh
+  git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+  chsh -s /bin/zsh
+  create_symlink .zshrc ~/
 }
 
 setup_brew() {
