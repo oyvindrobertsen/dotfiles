160b983e1e22c81a58a66aca04e609ea72a05927
Øyvind Robertsen <oyvind.robertsen@eika.no>
2018-02-09 15:55:48 +0100

    feat: show path in message buffer when moving around in JSON files

--- emacs/oyvind/config.el
+++ emacs/oyvind/config.el
@@ -4,3 +4,14 @@
 (require 'company)
 (setq company-idle-delay 0.2
       company-minimum-prefix-length 3)
+
+(defvar last-post-command-position 0 "Position in buffer after previous command.")
+(make-variable-buffer-local 'last-post-command-position)
+(defun show-json-path-on-position-change-hook ()
+  "Print JSON path to message buffer if position changed."
+  (unless (equal (point) last-post-command-position)
+    (jsons-print-path))
+  (setq last-post-command-position (point)))
+
+(add-hook 'json-mode-hook
+          (lambda () (add-hook 'post-command-hook #'show-json-path-on-position-change-hook nil 'local)))
